0ec76792a8a2ad070a6ec3c06a98c480bd1248e4
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2022-03-17 01:53:25 -0500

    successful loop to fight all enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,11 +11,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -35,15 +35,15 @@ var fight = function(enemyName) {
         playerHealth = playerHealth - enemyAttack;
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
-        );
+            );
         
-        // check player's health
+            // check player's health
         if (playerHealth <= 0) {
             window.alert(playerName + " has died!");
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-
+        
         // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
@@ -63,9 +63,13 @@ var fight = function(enemyName) {
     }else {
         window.alert("You need to choose a valid option. Try again!");
     }
-
+    }
+    
     
 };
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    // call fight function with enemy-robot
+    fight(pickedEnemyName);
 }
